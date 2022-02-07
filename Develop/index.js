f9a0d17ea222eacd2b197b5e77b4a2fcb56aebcc
john.a.clark3 <john.a.clark3@gmail.com>
2022-02-07 10:23:17 -0700

    Update index.js

--- Develop/index.js
+++ Develop/index.js
@@ -11,3 +11,16 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+
+
+// Modularize your code into multiple files
+
+// Write your code using ES6+ concepts, such as let, const, and arrow functions
+
+// Use npm (Node Package Manager) to initialize a project and install and import Node.js modules
+
+// Build an interactive command-line application that processes user input using a third-party Node.js module
+
+// Use string literals to dynamically generate markdown from the command line
