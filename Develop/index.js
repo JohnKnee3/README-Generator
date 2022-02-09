f9495b77bc455a433d2f1e443d58724f62db2698
john.a.clark3 <john.a.clark3@gmail.com>
2022-02-09 14:19:10 -0700

    Validate & When
    9.3.6
    Adds Validates to make sue you don't accidentally skip fields.  Also used a when to only display if a previous question returns a true.

--- Develop/index.js
+++ Develop/index.js
@@ -49,3 +49,4 @@ init();
 //9.2.6 created a new JS to handle the generate HTML function and pasted it in.  I then used mudule.exports = generatePage (the funcs name) to send it out.  Then in the app.js I used const generatePage = require('./src/page-template.js'); to catch it and set it to the var generatePage.
 //9.3.4 Used npm init to give my code access to the npm files.   Then selected the inquirer file from there website and installed it using the code npm install inquirer.  We are one step away from using it in app.js.  Lots of good info explaining the basics.
 //9.3.5 Made questions with inquirer.prompt and followed it's format.  Then got weird with adding the ability to make the questions again if chosen.  Everything gets console.log'd still.  Very DENSE STUFF.
+//9.3.6 Adds Validates to make sue you don't accidentally skip fields.  Also used a when to only display if a previous question returns a true.
