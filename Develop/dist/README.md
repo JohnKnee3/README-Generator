3c98cdaf4302fad19be30f7c73247a27a364bedb
john.a.clark3 <john.a.clark3@gmail.com>
2022-02-11 14:33:49 -0700

    Got all the Licenses in

--- Develop/dist/README.md
+++ Develop/dist/README.md
@@ -17,15 +17,9 @@
   ## Usage 
   a
 
-  ## License 
-  
-  MIT License
-
-  A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.
   
   
   
-  <a href= https://choosealicense.com/licenses/mit/>License Link</a>
   
   
   ## Contributing
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,6 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  console.log(license);
   if (license === "MIT") {
     return `![test](https://img.shields.io/badge/license-MIT-blue)`;
   } else if (license === "Apache") {
@@ -14,27 +13,44 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  return `
-  <a href= https://choosealicense.com/licenses/mit/>License Link</a>
-  `;
+  if (license === "MIT") {
+    return `
+  <a href= https://choosealicense.com/licenses/mit/>MIT License Link</a>`;
+  } else if (license === "Apache") {
+    return `
+  <a href= https://choosealicense.com/licenses/apache-2.0/> Apache License Link</a>`;
+  } else if (license === "None") {
+    return "";
+  }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  return `
-  MIT License
+  console.log(license);
+  if (license === "MIT") {
+    return `## License 
+  MIT 
 
   A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.
   `;
+  } else if (license === "Apache") {
+    return `## License
+  Apache 
+
+  A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+  `;
+  } else if (license === "None") {
+    return "";
+  }
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `## License 
-  ${renderLicenseSection()}
+  return `
+  ${renderLicenseSection(data)}
   ${renderLicenseBadge(data)}
-  ${renderLicenseLink()}
+  ${renderLicenseLink(data)}
   `;
 }
 
